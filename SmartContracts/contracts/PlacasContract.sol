f8590d2f7745001f2745ee9e16b76f28c72392a3
Gustavove <gustavove510@gmail.com>
2021-12-11 16:18:31 +0100

    Cambios en la organizacion del proyecto

---
+++ SmartContracts/contracts/PlacasContract.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+
+contract PlacasContract {
+
+    bytes32 hash_bd;
+    uint id_placa;
+    uint datos_co2;
+    address creador;
+
+    constructor(uint _id_placa, uint _datos_co2, bytes32 _hash_bd) public{
+        creador = msg.sender;
+        id_placa = _id_placa;
+        datos_co2 = _datos_co2;
+        hash_bd = _hash_bd;
+    }
+    
+    modifier soloCreador(){
+        require(msg.sender == creador);
+        _;
+    }
+    
+     function set_datos(uint _id_placa, uint _datos_co2, bytes32 _hash_bd) public soloCreador {
+        id_placa = _id_placa;
+        datos_co2 = _datos_co2;
+        hash_bd = _hash_bd;
+    }
+    
+    function getIdPlaca() public view returns (uint) {
+        return id_placa;
+    }
+    
+    function getHashBd() public view returns (bytes32) {
+        return hash_bd;
+    }
+    
+     function getDatosCo2() public view returns (uint) {
+        return datos_co2;
+    }
+
+}
---
+++ SmartContracts/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var placa = artifacts.require("PlacasContract");
+
+module.exports = function(deployer) {
+  deployer.deploy(placa);
+};
